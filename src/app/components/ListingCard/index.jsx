f6a620db56b8f22e20530e53b9807bb48a3f9b2f
jvtang487 <jvtang@ucsd.edu>
2024-04-06 14:45:03 -0700

    added listing card

--- src/app/components/ListingCard/index.jsx
+++ src/app/components/ListingCard/index.jsx
@@ -3,7 +3,12 @@ import styles from "./style.module.css";
 const ListingCard = () => {
   return (
     <div className={styles.card}>
-      
+      <img className={styles.card_img} src="./assets/item2.jpg"/>
+      <h2 className={styles.card_title}>Parrot</h2>
+      <p className={styles.price}>$20</p>
+      <Button className={styles.card_button}
+      Title = "Claim Me"
+      />
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import storeInfo from "./storeInfo";
 import styles from "./page.module.css";
+import ListingCard from "./components/ListingCard";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1 className={styles.store_header}>Pirate Marketplace</h1>
+      <ListingCard />
     </main>
   );
 }
